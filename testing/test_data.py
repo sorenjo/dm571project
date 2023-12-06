d7014f39fa0402c9de42897d8e61e236b04c1891
soren <>
2023-12-06 19:40:24 +0100

    testing

--- testing/test_data.py
+++ testing/test_data.py
@@ -24,11 +24,27 @@ class TestUser( unittest.TestCase ):
         self.assertTrue( [ *tr ] == [ ( "lars", "testshow", date ) ])
         self.assertTrue( [ *btn ] == [ ( f"/untake/{sid}", "Untake Shift" ) ])
 
-    def test_add_show( self ):
-        pass
+    def test_shows_table( self ):
+        data = Data()
+        data.add_user( "lars", "123", True )
+        date = datetime( 2023, 12, 8, hour=9 )
+        sid = data.add_show( "testshow", date, 90, 4)
+        data.take( "lars", sid )
+        tr, btn = data.shows_table()
+        self.assertTrue( [ *tr ] == [ ( "testshow", date, 90, "1 / 4" ) ] )
 
     def test_show_detail( self ):
-        pass
+        data = Data()
+        data.add_user( "lars", "123", True )
+        date = datetime( 2023, 12, 8, hour=9 )
+        sid = data.add_show( "testshow", date, 90, 4)
+        data.take( "lars", sid )
+        tr, disabled = data.show_detail( sid, "lars" )
+        self.assertTrue( [ *tr ] == [ ( "lars", ) ] )
+        self.assertTrue( disabled )
 
-    def test_untake( self ):
-        pass
+    def test_login( self ):
+        data = Data()
+        data.add_user( "lars", "123", True )
+        self.assertTrue( data.login( "lars", "123" ) )
+        
